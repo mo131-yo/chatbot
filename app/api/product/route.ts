8d16bf48a46c5bf7614d00580ecde5564615ebc8
25LP7053 25LP7053 <johndoe@example.com>
2026-03-25 13:16:38 +0800

    AS

---
+++ app/api/product/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest } from "next/server";
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+  stock?: number;
+  color?: string;
+  size?: string;
+  rating?: number;
+  reviews?: number;
+  category?: string;
+  brand?: string;
+};
+
+export async function POST(request: NextRequest) {
+  const { name, price, description, imageUrl } = await request.json();
+
+
+
+  try {
+    const newProduct: Product = {
+      id: Math.floor(Math.random() * 1000),
+      name,
+      price,
+      description,
+      imageUrl,
+      stock: 0,
+      size: "",
+      rating: 0,
+      reviews: 0,
+      category: "",
+      brand: "",
+    };
+
+
+
+    return new Response(JSON.stringify(newProduct), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to create product" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
