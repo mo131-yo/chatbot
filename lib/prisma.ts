602ecf297fcbc412a949ea4683695fa8fb8e7cb8
25LP7053 25LP7053 <johndoe@example.com>
2026-03-26 13:26:59 +0800

    uurchlult

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,12 @@
-import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { Pool } from "@neondatabase/serverless";
- 
+import { PrismaClient } from "@prisma/client";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
- 
+
 const adapter = new PrismaNeon(pool as any);
- 
+
 export const prisma = new PrismaClient({
   adapter,
 });
