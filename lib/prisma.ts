be1e85ee75b2db3eb237db5abe75d5f23e22be42
Administrator <munhorgilmunhtsetseg333@gmail.com>
2026-03-26 10:02:23 +0800

    againa fixed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,13 @@
-// @ts-ignore
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: any;
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["query"],
-    // @ts-ignore
-    engineType: "library", 
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
