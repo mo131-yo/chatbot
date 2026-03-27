db9e314cf12973bf9fa96b54dbd22c5e9cfe4c48
Administrator <munhorgilmunhtsetseg333@gmail.com>
2026-03-27 09:30:27 +0800

    vercel

--- app/chat/products/api/productCreate/route.ts
+++ app/chat/products/api/productCreate/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
       rating,
       reviews,
       category,
-      brand,
+      brand,  
     } = body;
 
     const product = await prisma.product.create({
