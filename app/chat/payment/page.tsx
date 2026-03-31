b41e24d3a0717cb68408468b0a9a17d43e345726
Administrator <munhorgilmunhtsetseg333@gmail.com>
2026-03-31 10:03:27 +0800

    AS

---
+++ app/chat/payment/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PaymentPage() {
+  return (
+    <div className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-3xl font-bold">Төлбөрийн хэсэг</h1>
+    </div>
+  );
+}
