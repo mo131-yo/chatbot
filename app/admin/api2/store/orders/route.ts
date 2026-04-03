c1f97d19745d7b72adbf7d06a843330db7478625
Administrator <munhorgilmunhtsetseg333@gmail.com>
2026-04-03 08:28:57 +0800

    AS

--- app/admin/api2/store/orders/route.ts
+++ app/admin/api2/store/orders/route.ts
@@ -1,13 +1,24 @@
-import {prisma} from "@/lib/prisma";
-import { url } from "inspector";
-import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  const storeId = new URL(req.url).searchParams.get("storeId");
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const storeId = searchParams.get("storeId");
 
-  const orders = await prisma.order.findMany({
-    where: { storeId: storeId || undefined },
-    include: { product: true },
-  });
-  return NextResponse.json(orders);
+    const orders = await prisma.order.findMany({
+      where: storeId ? { storeId } : {},
+      include: { 
+        product: true 
+      },
+      orderBy: {
+        createdAt: "desc"
+      }
+    });
+
+    return NextResponse.json(orders);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Захиалга авахад алдаа гарлаа" }, { status: 500 });
+  }
 }
