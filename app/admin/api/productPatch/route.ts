5c63a27e03e9b823451057fa2f4bc6123900c96d
Administrator <munhorgilmunhtsetseg333@gmail.com>
2026-04-03 08:14:46 +0800

    AS

--- app/admin/api/productPatch/route.ts
+++ app/admin/api/productPatch/route.ts
@@ -1,13 +1,15 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PATCH(
-  req: NextRequest,
-  { params }: { params: Promise<{ id: string }> },
-) {
+export async function PATCH(req: NextRequest) {
   try {
-    const resolvedParams = await params;
-    const id = resolvedParams.id;
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+
+    if (!id) {
+      return NextResponse.json({ error: "ID шаардлагатай" }, { status: 400 });
+    }
+
     const body = await req.json();
 
     const product = await prisma.product.update({
