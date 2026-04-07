759ef0ee9de23575f8552c192a2f04bd28c057ff
25LP7053 25LP7053 <johndoe@example.com>
2026-04-07 10:42:52 +0800

    AS

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,22 @@
+// "use client";
+ 
+// import ProductTable from "@/app/admin/components/product/ProductTable";
+// import ProductForm from "@/app/admin/components/product/ProductForm";
+ 
+// export default function ProductsPage() {
+//   return (
+//     <div>
+//       <div className="flex justify-between mb-4">
+//         <h1 className="text-xl">Products</h1>
+//         <ProductForm onSuccess={() => location.reload()} />
+//       </div>
+//  {/* dasdas */}
+//       <ProductTable />
+//     </div>
+//   );
+// }
+
+
 "use client";
  
 import ProductTable from "@/app/admin/components/product/ProductTable";
@@ -10,8 +29,9 @@ export default function ProductsPage() {
         <h1 className="text-xl">Products</h1>
         <ProductForm onSuccess={() => location.reload()} />
       </div>
- {/* dasdas */}
+{/* dasdas */}
       <ProductTable />
     </div>
   );
 }
+ 
